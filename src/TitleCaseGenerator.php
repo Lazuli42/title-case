4bd9b9a2f17da7bd928974f2bdd9d9115e458f2b
andres garcia and zachary baum <andres@>
2016-09-12 11:32:37 -0400

    add ability to not capitalize article according to the third spec

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -6,8 +6,18 @@
         {
             $input_array_of_words = explode(" ", $input_title);
             $output_titlecased = array();
+            $articles = ["for", "and", "nor", "but", "or", "yet", "so", "at", "but", "by", "of", "to"];
             foreach ($input_array_of_words as $word) {
-                array_push($output_titlecased, ucfirst($word));
+                $word = strtolower($word);
+                foreach ($articles as $article) {
+                    if ($word !== $article) {
+                        $cased_word = ucfirst($word);
+                    }
+                    elseif ($word == $article) {
+                        $cased_word = $word;
+                    }
+                }
+                array_push($output_titlecased, $cased_word);
             }
             return implode(" ", $output_titlecased);
         }
