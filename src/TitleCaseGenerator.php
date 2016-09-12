b2090cf8ac8b610874b7e37bb2d2248c6b8b41e8
andres garcia and zachary baum <zachary@>
2016-09-12 10:52:33 -0400

    add front end logic for the first two specs

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -5,12 +5,11 @@
         function makeTitleCase($input_title)
         {
             $input_array_of_words = explode(" ", $input_title);
-            $output_titledcased = array();
+            $output_titlecased = array();
             foreach ($input_array_of_words as $word) {
-                array_push($output_titledcased, ucfirst($word));
-
+                array_push($output_titlecased, ucfirst($word));
             }
-            return implode (" ", $output_titledcased);
+            return implode(" ", $output_titlecased);
         }
     }
 
